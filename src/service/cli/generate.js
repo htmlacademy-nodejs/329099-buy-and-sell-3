ca280a6b4174ba11ab37b3b55dcf7ded334c34d0
Edwin18 <igreenbergi78@gmail.com>
2021-03-26 18:44:54 +0200

    Moved mock data to files. Updated functionality to read them.

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const fs = require(`fs`).promises;
+const path = require(`path`);
 const chalk = require('chalk');
 const {ExitCode} = require(`../../constants`);
 const {
@@ -8,70 +9,55 @@ const {
   shuffle,
 } = require(`../../utils`);
 
-const Count = {
-  DEFAULT: 1,
-  MAX: 1000,
-}
+
 const FILE_NAME = `mocks.json`;
-
-const TITLES = [
-  `Продам книги Стивена Кинга`,
-  `Продам новую приставку Sony Playstation 5`,
-  `Продам отличную подборку фильмов на VHS`,
-  `Куплю антиквариат`,
-  `Куплю породистого кота`,
-];
-
-const SENTENCES = [
-  `Товар в отличном состоянии.`,
-  `Пользовались бережно и только по большим праздникам.`,
-  `Продаю с болью в сердце...`,
-  `Бонусом отдам все аксессуары.`,
-  `Даю недельную гарантию.`,
-  `Если товар не понравится — верну всё до последней копейки.`,
-  `Это настоящая находка для коллекционера!`,
-  `Если найдёте дешевле — сброшу цену.`,
-  `Таких предложений больше нет!`,
-  `При покупке с меня бесплатная доставка в черте города.`,
-];
-
-const CATEGORIES = [
-  `Книги`,
-  `Разное`,
-  `Посуда`,
-  `Игры`,
-  `Животные`,
-  `Журналы`,
-];
-
+const DATA_DIR = `data`;
+const CATEGORIES_FILE = `categories.txt`;
+const SENTENCES_FILE = `sentences.txt`;
+const TITLES_FILE = `titles.txt`;
 const OfferType = {
   OFFER: `offer`,
   SALE: `sale`,
 };
-
-
+const Count = {
+  DEFAULT: 1,
+  MAX: 1000,
+};
 const SumRestrict = {
   MIN: 1000,
   MAX: 100000,
 };
-
 const PictureRestrict = {
   MIN: 1,
   MAX: 16,
 };
 
+const getData = async (fileName) => {
+  try {
+    const filePath = path.join(process.cwd(), DATA_DIR, fileName);
+    const file = await fs.readFile(filePath);
+    return file.toString().split(`\n`).slice(0, -1);
+  } catch (error) {
+    console.error(`Can't open file...\n${error}`)
+  }
+};
+
 const getPictureFileName = (number) => `item${number.toString().padStart(2, 0)}.jpg`;
 
-const generateOffers = (count) => (
-  Array(count).fill({}).map(() => ({
-    category: shuffle(CATEGORIES).slice(1, getRandomInt(2, CATEGORIES.length - 1)),
-    description: shuffle(SENTENCES).slice(1, 5).join(` `),
+const generateOffers = async (count) => {
+  const categories = await getData(CATEGORIES_FILE);
+  const sentences = await getData(SENTENCES_FILE);
+  const titles = await getData(TITLES_FILE);
+
+  return Array(count).fill({}).map(() => ({
+    category: shuffle(categories).slice(1, getRandomInt(2, categories.length - 1)),
+    description: shuffle(sentences).slice(1, 5).join(` `),
     picture: getPictureFileName(getRandomInt(PictureRestrict.MIN, PictureRestrict.MAX)),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    title: titles[getRandomInt(0, titles.length - 1)],
     type: OfferType[Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)]],
     sum: getRandomInt(SumRestrict.MIN, SumRestrict.MAX),
   }))
-);
+};
 
 module.exports = {
   name: `--generate`,
@@ -84,7 +70,7 @@ module.exports = {
     }
 
     const countOffer = Number.parseInt(count, 10) || Count.DEFAULT;
-    const content = JSON.stringify(generateOffers(countOffer));
+    const content = JSON.stringify(await generateOffers(countOffer));
 
     try {
       await fs.writeFile(FILE_NAME, content);
