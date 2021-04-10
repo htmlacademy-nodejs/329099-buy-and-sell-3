13aba41f3d9339f8b460aa311634615ea9a0d55b
Edwin18 <igreenbergi78@gmail.com>
2021-04-10 15:34:29 +0300

    WIP: Current state.

--- src/express/express.js
+++ src/express/express.js
@@ -1,7 +1,8 @@
-'use strict';
+`use strict`;
 
 const express = require(`express`);
 const chalk = require(`chalk`);
+const path = require(`path`);
 const loginRouter = require(`./routes/login`);
 const mainRouter = require(`./routes/main`);
 const myRouter = require(`./routes/my`);
@@ -9,9 +10,22 @@ const offersRouter = require(`./routes/offers`);
 const registerRouter = require(`./routes/register`);
 const searchRouter = require(`./routes/search`);
 
+const PUBLIC_DIR = `public`;
+const TEMPLATE_DIR = `templates`;
 const DEFAULT_PORT = 8080;
 
 const app = express();
+
+app.set(`views`, path.resolve(__dirname, TEMPLATE_DIR));
+app.set(`view engine`, `pug`);
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+// Это какой-то лютый костыль либо я делаю что-то не так.
+// app.use(`/login`, express.static(path.resolve(__dirname, PUBLIC_DIR)));
+// app.use(`/my`, express.static(path.resolve(__dirname, PUBLIC_DIR)));
+// app.use(`/offers`, express.static(path.resolve(__dirname, PUBLIC_DIR)));
+// app.use(`/register`, express.static(path.resolve(__dirname, PUBLIC_DIR)));
+// app.use(`/search`, express.static(path.resolve(__dirname, PUBLIC_DIR)));
 
 app.use(`/`, mainRouter);
 app.use(`/login`, loginRouter);
